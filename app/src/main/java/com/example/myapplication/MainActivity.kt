608447890a73cb393adb9edc8f1efefafa397ea5
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-10-20 11:12:22 +0200

    break the project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,28 +1,17 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.example.myapplication.databinding.ActivityEdittextBinding
 
-class MainActivity : AppCompatActivity() {
+internal class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEdittextBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
-
-        fab.setOnClickListener {
-            Log.i("FAB", "Clicked!")
-            lifecycleScope.launch {
-                it.isEnabled = false
-                delay(2000)
-                it.isEnabled = true
-            }
-        }
+        binding = ActivityEdittextBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
     }
 }
